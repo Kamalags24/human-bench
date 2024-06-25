2e9cba91af3e1c9cf69b8bb45b7acc2320334b6b
Fresnel <fresneljeanclaude@outlook.com>
2024-06-25 15:57:33 +0200

    infopage

---
+++ src/components/information.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>This is Where the Ad displays!</p>
+  </div>
+  <div>
+    <div>
+        <h2>Aim Trainer</h2>
+        <p>Click the targets as quickly and accurately as you can.</p>
+        <p>This tests reflexes and hand-eye coordination.</p>
+        <p>Once you've clicked 30 targets, your score and average time per target will be displayed.</p>
+        <p>Scores in this test are slower than the simple reaction time test, because you must react and then move the cursor.</p>
+        <p>This test is best taken with a mouse or tablet screen. Trackpads are difficult to score well with.</p>
+    </div>
+
+    <div>
+        <h2>Statistics</h2>
+        <img src="public\statt.png" alt="">
+    </div>
+
+    <div>
+        <h2>This is Where Another Ad displays!</h2>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>

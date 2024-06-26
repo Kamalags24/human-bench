a0f6f6d7b564fee3d9ea73b7499ed4b70906add2
Fresnel <fresneljeanclaude@outlook.com>
2024-06-26 13:01:27 +0200

    barre,input

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
 <script setup>
+import { RouterLink, RouterView } from "vue-router";
 import gameArea from "@/components/gameArea.vue";
 import information from "@/components/information.vue";
 </script>
 
 <template>
   <div>
-    <nav>
-      <router-link to="/">HUMAN BENCHMARK</router-link>
-      <router-link to="/">DASHBOARD</router-link>
-      <router-link to="/">SIGN UP</router-link>
-      <router-link to="/">LOGIN</router-link>
+    <nav class="barre">
+      <div class="rl1">
+        <router-link class="l1" to="/">HUMAN BENCHMARK</router-link>
+        <router-link to="/">DASHBOARD</router-link>
+      </div>
+      <div class="rl2">
+        <router-link class="l2" to="/">SIGN UP</router-link>
+        <router-link to="/">LOGIN</router-link>
+      </div>
     </nav>
   </div>
   <gameArea />
   <information />
+  <RouterView />
 </template>
 
 <style scoped>
+.barre {
+  margin: 0;
+  padding: 0;
+  justify-content: center;
+  display: flex;
+  gap: 20%;
+  font-size: 20px;
+  margin: 18px;
+}
+
+.rl1 {
+  justify-content: space-between;
+  gap: 2%;
+}
+
+.l1 {
+  margin-right: 25px;
+}
+
+.rl2 {
+  /* display: flex; */
+  gap: 5%;
+  margin-right: 20px;
+}
+
+.l2 {
+  margin-right: 20px;
+}
+
+
+a {
+ color: black;
+text-decoration: none;
+padding: 10px;
+
+}
+
+a:hover {
+  background-color: #e6e6e6;
+  
+}
 
 </style>
--- src/components/gameArea.vue
+++ src/components/gameArea.vue
@@ -6,13 +6,15 @@ import { ref } from "vue";
 const Start = ref(true);
 const Game = ref(true);
 const End = ref(true);
-let count = ref(5);
+let count = ref();
 let left = ref(0);
 let top = ref(0);
 let second = ref(0);
 let score = ref(0);
+const numClicks = ref(5);
 
 function startGame() {
+  count.value = numClicks.value
   Start.value = false;
   Game.value = false;
   End.value = true;
@@ -55,11 +57,18 @@ console.log("Le temps passé est : ", second.value);
 <template>
   <div class="container">
     <div v-if="Start">
-      <h1>Bienvenue dans le jeu</h1>
-      <div class="cercle" @click="startGame"></div>
+      <h1>Bienvenue dans le jeu !</h1>
+      <div class="cercle" ></div>
       <p>
-        Hit 30 targets as quickly as you can. Click the target above to begin
+        Choisissez le nombre de cible et atteigner le, le plus vite possible !
       </p>
+      <!-- <input type="number"> -->
+      <div class="input-button">
+          <p class="label">Nombre de clics minimum (minimum 5):</p><br>
+          <input type="number" id="clicks" v-model="numClicks" min="5" @input="validateNumClicks"><br>
+          <button @click="startGame" :disabled="numClicks < 5" class="gamebutton">Commencer le jeu</button>
+      </div>
+      
     </div>
 
     <main class="dashboard" v-if="!Game">
@@ -75,16 +84,141 @@ console.log("Le temps passé est : ", second.value);
 
     <div v-if="!End">
       <div class="cercle"></div>
-      <p>Average time per target</p>
-      <p>{{ second }}</p>
-      <p>Save your score to see how you compare</p>
-      <button @click="restartGame">Try again</button>
+      <p class="cercle-p">Temps moyen par click</p>
+      <p class="second">{{ second + " ms" }}</p>
+      <p class="save">Save your score to see how you compare</p>
+      <button class="restast-button" @click="restartGame">Reprendre</button>
     </div>
   </div>
 </template>
 
 
 <style scoped>
+.cercle-p {
+  color: white;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  font-size: 28px;
+  margin-top: 10px;
+}
+
+.restast-button {
+  margin: 0;
+  padding: 13px;
+  text-align: center;
+  transform: translateX(130px);
+  margin-top: 35px;
+  color: inherit;
+  background-color: yellow;
+  border-radius: 10px;
+  border: none;
+  color: black;
+  font-weight: bold;
+}
+
+.save {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 25px;
+}
+
+.second {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  font-weight: bold;
+  font-size: 60px;
+  font-family: sans-serif;
+}
+
+.container {
+  width: 100%;
+  height: 500px;
+  background-color: #2b87d1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+h1 {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.input-button {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+main h1 {
+  margin-bottom: 100px;
+}
+
+.cercle {
+  background-color: #ff000080;
+  border: 1px solid #ff000080;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  position: relative;
+  top: 0;
+  left: 0;
+  margin: auto;
+}
+
+input {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+  /* margin-top: 70px; */
+  margin-bottom: 10px;
+}
+
+.label {
+  margin-bottom: 8px;
+  margin-top: 8px;
+  font-size: 20px;
+}
+
+.gamebutton {
+  color: inherit;
+  background-color: yellow;
+  border-radius: 10px;
+  border: none;
+  color: black;
+  font-weight: bold;
+  padding: 8px;
+  margin-top: 20px;
+}
+
+.gamebutton:hover {
+  cursor: pointer
+}
+
+
+/* h1 {
+  margin: 0 auto;
+  color: white;
+}
+
+.gamebutton {
+  margin-top: 30px;
+}
+
+label {
+  color: white;
+  padding-top: 29px;
+}
+
 .container {
   background-color: #2b87d1;
   display: flex;
@@ -93,6 +227,19 @@ console.log("Le temps passé est : ", second.value);
   flex-direction: column;
   width: 100%;
   height: 500px;
+}
+
+.container p {
+  color: white;
+}
+
+input {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  margin-top: 70px;
 }
 
 .container-cercle {
@@ -124,5 +271,5 @@ button {
   border: 1px solid black;
   height: 300px;
   width: 800px;
-}
+} */
 </style>
